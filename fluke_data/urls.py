7fd812226a7e8652a9ce384cd4f01cf0816c0589
calassararct <rafaelcalassara@hotmail.com>
2024-07-31 10:33:00 -0300

    created manage template and views

--- fluke_data/urls.py
+++ fluke_data/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get_thermohygrometers/', views.get_thermohygrometers, name='get_thermohygrometers'),
+    path('manage_thermohygrometers/', views.manage_thermohygrometers, name='manage_thermohygrometers'),
+    path('api/add_thermohygrometer/', views.add_thermohygrometer, name='add_thermohygrometer'),
+    path('api/delete_thermohygrometer/<int:id>/', views.delete_thermohygrometer, name='delete_thermohygrometer'),
 ]
--- fluke_data/views.py
+++ fluke_data/views.py
@@ -1,6 +1,9 @@
 # fluke_data/views.py
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import ThermohygrometerModel
 
 def index(request):
@@ -17,3 +20,31 @@ def get_thermohygrometers(request):
         }
     for thermo in thermohygrometers]
     return JsonResponse(data, safe=False)
+
+def manage_thermohygrometers(request):
+    thermohygrometers = ThermohygrometerModel.objects.all()
+    return render(request, 'fluke_data/manage_thermohygrometers.html', {'thermohygrometers': thermohygrometers})
+
+
+@csrf_exempt
+def add_thermohygrometer(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        instrument_name = data.get('instrument_name')
+        pn = data.get('pn')
+        sn = data.get('sn')
+        if instrument_name and pn and sn:
+            ThermohygrometerModel.objects.create(instrument_name=instrument_name, pn=pn, sn=sn)
+            return JsonResponse({'success': True})
+        return JsonResponse({'success': False, 'error': 'Missing fields'})
+
+@csrf_exempt
+def delete_thermohygrometer(request, id):
+    if request.method == 'DELETE':
+        try:
+            thermo = ThermohygrometerModel.objects.get(id=id)
+            thermo.delete()
+            return JsonResponse({'success': True})
+        except ThermohygrometerModel.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'Thermohygrometer not found'})
+
