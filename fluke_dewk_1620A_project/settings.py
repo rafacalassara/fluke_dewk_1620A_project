82b3bd5859736356710a75f826ba8378e5b246e9
calassararct <rafaelcalassara@hotmail.com>
2024-08-06 10:53:43 -0300

    removed celery configurations

--- fluke_dewk_1620A_project/settings.py
+++ fluke_dewk_1620A_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,18 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Celery Settings
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-
-CELERY_BEAT_SCHEDULE = {
-    'record-measurements-every-2-minutes': {
-        'task': 'fluke_data.tasks.record_measurements',
-        'schedule': 120.0,  # every 2 minutes
-    },
-}
 
 # Channels settings
 
