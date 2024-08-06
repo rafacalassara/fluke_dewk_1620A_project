2c72090bda3a851bf68c3b9d84a58d578e92faa6
calassararct <rafaelcalassara@hotmail.com>
2024-08-06 10:55:34 -0300

    add function in consumers to handle database saving every "n" minutes

--- fluke_data/consumers.py
+++ fluke_data/consumers.py
@@ -1,38 +1,31 @@
+# fluke_data/consumers.py
+
 import json
 import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.layers import get_channel_layer
-from .visa_communication import Instrument
 from asgiref.sync import sync_to_async
-from .models import ThermohygrometerModel
+from datetime import datetime, timedelta
+from .visa_communication import Instrument
+from .models import ThermohygrometerModel, MeasuresModel
 
 class DataConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.thermohygrometer_id = self.scope['url_route']['kwargs']['thermohygrometer_id']
-        self.instrument = await sync_to_async(self.get_instrument)()
-        self.group_name = f"thermo_{self.thermohygrometer_id}"
-        self.running = True
-
-        if self.instrument.instrument:
-            await self.channel_layer.group_add(
-                self.group_name,
-                self.channel_name
-            )
+        await self.initialize_consumer()
+        if self.instrument and self.instrument.instrument:
+            await self.add_to_group()
             await self.accept()
-            await self.send(text_data=json.dumps({'message': 'Connecting...'}))
+            await self.send_connecting_message()
+            await sync_to_async(self.update_connection_status)(True)
             asyncio.create_task(self.send_data_loop())
         else:
-            await self.send(text_data=json.dumps({'message': 'Failed to connect'}))
+            await self.send_failure_message()
             await self.close()
 
     async def disconnect(self, close_code):
         self.running = False
-        if hasattr(self.instrument, 'disconnect'):
-            await sync_to_async(self.instrument.disconnect)()
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
+        await self.disconnect_instrument()
+        await self.remove_from_group()
+        await sync_to_async(self.update_connection_status)(False)
 
     async def receive(self, text_data):
         message = json.loads(text_data)
@@ -40,36 +33,87 @@ class DataConsumer(AsyncWebsocketConsumer):
             await self.close()
 
     async def send_data_loop(self):
+        self.last_saved_time = None
         while self.running:
             try:
                 data = await sync_to_async(self.instrument.get_data)()
                 if data:
-                    await self.channel_layer.group_send(
-                        self.group_name,
-                        {
-                            'type': 'thermo_data',
-                            'data': data,
-                        }
-                    )
+                    await self.broadcast_data(data)
+                    await self.check_and_save_data(data)
             except Exception as e:
-                await self.channel_layer.group_send(
-                    self.group_name,
-                    {
-                        'type': 'thermo_data',
-                        'error': str(e),
-                    }
-                )
+                await self.broadcast_error(str(e))
             await asyncio.sleep(1)
 
     async def thermo_data(self, event):
-        data = event.get('data', {})
-        error = event.get('error', None)
-        if error:
-            await self.send(text_data=json.dumps({'error': error}))
+        if 'error' in event:
+            await self.send(text_data=json.dumps({'error': event['error']}))
         else:
-            await self.send(text_data=json.dumps({'data': data}))
+            await self.send(text_data=json.dumps({'data': event['data']}))
+
+    # Helper Methods
+    async def initialize_consumer(self):
+        self.thermohygrometer_id = self.scope['url_route']['kwargs']['thermohygrometer_id']
+        self.instrument = await sync_to_async(self.get_instrument)()
+        self.group_name = f"thermo_{self.thermohygrometer_id}"
+        self.running = True
+
+    async def add_to_group(self):
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def remove_from_group(self):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def send_connecting_message(self):
+        await self.send(text_data=json.dumps({'message': 'Connecting...'}))
+
+    async def send_failure_message(self):
+        await self.send(text_data=json.dumps({'message': 'Failed to connect'}))
+
+    async def broadcast_data(self, data):
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'thermo_data',
+                'data': data,
+            }
+        )
+
+    async def broadcast_error(self, error):
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'thermo_data',
+                'error': error,
+            }
+        )
+
+    async def disconnect_instrument(self):
+        if hasattr(self.instrument, 'disconnect'):
+            await sync_to_async(self.instrument.disconnect)()
 
     def get_instrument(self):
-        thermo = ThermohygrometerModel.objects.get(id=self.thermohygrometer_id)
-        instrument = Instrument(thermo.ip_address)
-        return instrument
+        self.thermo = ThermohygrometerModel.objects.get(id=self.thermohygrometer_id)
+        return Instrument(self.thermo.ip_address)
+
+    async def check_and_save_data(self, data):
+        current_time = datetime.strptime(data['date'], '%Y/%m/%d %H:%M:%S')
+        if self.last_saved_time is None or current_time >= self.last_saved_time + timedelta(minutes=1):
+            await sync_to_async(self.save_data_to_db)(data)
+            self.last_saved_time = current_time
+
+    def save_data_to_db(self, data):
+        MeasuresModel.objects.create(
+            instrument=self.thermo,
+            temperature=data['temperature'],
+            humidity=data['humidity'],
+            date=datetime.strptime(data['date'], '%Y/%m/%d %H:%M:%S')
+        )
+
+    def update_connection_status(self, status):
+        ThermohygrometerModel.objects.filter(id=self.thermohygrometer_id).update(is_connected=status)
